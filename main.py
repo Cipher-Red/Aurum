565dfed9a09abb0fc52f10e358c2ba2e82b7f31d
Cipher-Red <Qipher09@proton.me>
2025-02-20 06:09:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,58 +15,36 @@ def validate(email):
         # If the Email is valid then it will run this function
         valid = validate_email(email)
         normalized_email = valid.email
-        return Fore.GREEN + f"The Email Address |{normalized_email}| is a valid email address."
+        return Fore.GREEN + f"The Email Address {normalized_email} is a valid email address."
 
 
     # Email is not valid it will run this function
     except EmailNotValidError as e:
-        return Fore.RED + f"The Email Address |{email}| is not a valid email address {e}."
+        return Fore.RED + f"The Email Address {email} is not a valid email address {e}."
 
 def mx_records(email):
-
     try:
         # Validate email syntax
         valid = validate_email(email)
         normalized_email = valid.email
         domain = normalized_email.split("@")[1]
+        records = ["MX", "A", "AAAA", "CNAME", "NS", "PTR", "SOA", "TXT", "SRV", "TLSA", "CAA", "DNAME"]
 
-        try:
-            # Check if the domain has MX records
-            mx_records = dns.resolver.resolve(domain, 'MX')
-            return Fore.GREEN + f"Domain |{domain}| has MX records. This email can receive emails."
-            return
-        except LifetimeTimeout:
-            pass
-        except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.LifetimeTimeout):
-            return Fore.RED + f"Domain |{domain}| does not have MX records."
+        # Check if the domain has any of the required records
+        for record in records:
+            try:
+                answers = dns.resolver.resolve(domain, record, lifetime=15)  # Timeout in 3 seconds
+                if answers:
+                    return Fore.GREEN + f"Domain {domain} has a Valid {record} record. This email might receive emails."
+            except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.LifetimeTimeout, dns.resolver.NoNameservers):
+                continue
 
-        # No MX then check A
-        try:
-            dns.resolver.resolve(domain, 'A')
-            return f"No MX records, but |{domain}| has an A record. It might accept emails."
-            return
-        except LifetimeTimeout:
-            pass
-        except dns.resolver.NoAnswer:
-            pass
-
-        # No A check for AAAA
-        try:
-            dns.resolver.resolve(domain, 'AAAA')
-            return f"No MX records, but |{domain}| has an AAAA record. It might accept emails."
-            return
-        except LifetimeTimeout:
-            pass
-        except dns.resolver.NoAnswer:
-            pass
-
-        # If none have the domain then its invalid
-        return Fore.RED + f"Domain |{domain}| does not accept emails."
+        return Fore.RED + f"Domain {domain} does not have a valid DNS records."
 
     except EmailNotValidError as e:
-        return Fore.RED + f"Invalid email: {email} {e}"
+        return Fore.RED + f"Invalid email: {email} ({e})"
     except EmailUndeliverableError:
-        return Fore.RED + f"The domain |{domain}| does not accept email."
+        return Fore.RED + f"The domain {domain} does not accept email."
 
 def blacklistcheck(email):
     try:
@@ -87,14 +65,14 @@ def blacklistcheck(email):
             query = f"{domain}.{blacklist}"
             try:
                 dns.resolver.resolve(query, "A")
-                return Fore.RED + f"The Domain |{domain}| is Blacklisted on {blacklist}"
+                return Fore.RED + f"The Domain {domain} is Blacklisted on {blacklist}"
                 return
             except LifetimeTimeout:
                 return f"DNS query for {domain} timed out."
                 continue
             except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
                 continue
-        return Fore.GREEN + f"The domain |{domain}| is not blacklisted on any of the checked blacklists."
+        return Fore.GREEN + f"The domain {domain} is not blacklisted on any of the checked blacklists."
 
     except EmailNotValidError as e:
         return Fore.RED + f"Email is in an invalid form: {e}"
@@ -111,10 +89,10 @@ def cblacklist(email):
         blacklist = ["0SPAM","Abuse.ro","example.domain"]
 
         if domain in blacklist or normalized_email in emailbl:
-            return Fore.RED + f"This Email address |{email}| is blacklisted."
+            return Fore.RED + f"This Email address {email} is blacklisted."
 
         else:
-            return Fore.GREEN + f"The Email address |{email}| isn't blacklisted internally"
+            return Fore.GREEN + f"The Email address {email} isn't blacklisted internally"
 
     except EmailNotValidError as e:
         return Fore.RED + f"Email is in an invalid form {e}"
@@ -124,7 +102,7 @@ def fullscan(email):
     print(Style.RESET_ALL + "[Validating Email Address]".center(terminal_width, ' '))
     print(validate(email))
 
-    print(Style.RESET_ALL + "[Checking MX records]".center(terminal_width, ' '))
+    print(Style.RESET_ALL + "[Checking DNS records]".center(terminal_width, ' '))
     print(mx_records(email))
 
     print(Style.RESET_ALL + "[Checking if the domain is blacklisted]".center(terminal_width, ' '))
@@ -140,22 +118,22 @@ def rct(text):
 def bulfullscan(email):
     output_data = []
 
-    output_data.append("\n[Validation Result]\n")
+    output_data.append("\n~~~~Validation Result~~~~\n")
     result = validate(email) or "No result"  # Ensure something is returned
     result = rct(result)
     output_data.append(result)
 
-    output_data.append("\n[MX records Result]\n")
+    output_data.append("\n~~~~DNS records Result~~~~\n")
     result = mx_records(email) or "No result"
     result = rct(result)
     output_data.append(result)
 
-    output_data.append("\n[Blacklist Result]\n")
+    output_data.append("\n~~~~Blacklist Result~~~~\n")
     result = blacklistcheck(email) or "No result"
     result = rct(result)
     output_data.append(result)
 
-    output_data.append("\n[Custom blacklist Result]\n")
+    output_data.append("\n~~~~Custom blacklist Result~~~~\n")
     result = cblacklist(email) or "No result"
     result = rct(result)
     output_data.append(result)
@@ -173,11 +151,10 @@ def report(output_data, filename="Aurum_Report.pdf"):
 
     for line in output_data:
 
-        if "Email:" in line:  # Assuming the email is a part of this line
-            pdf.set_font("Arial", style='B', size=12)  # Set bold
-
+        if "Email:" in line:
+            pdf.set_font("Arial", style='B', size=15)
         else:
-            pdf.set_font("Arial", size=12)  # Set normal font
+            pdf.set_font("Arial", size=13)  # Set normal font
 
         pdf.multi_cell(0, 10, line)
 
